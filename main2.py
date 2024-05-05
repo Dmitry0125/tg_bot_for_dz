d5aa8f3d5638427cccbc8e63b13d94c724e53d14
Dmitry0125 <dmitry_mb@list.ru>
2024-05-05 23:00:22 +0700

    Добавление кнопки "Расписание" и её обработки + сообщение "спасибо"

--- main2.py
+++ main2.py
@@ -33,8 +33,9 @@ def start(message):
     btn2 = types.KeyboardButton('Другая дата')
     markup.row(btn1, btn2)
     btn3 = types.KeyboardButton('Донат')
+    btn4 = types.KeyboardButton('Расписание')
     # btn4 = types.KeyboardButton('Дз за всё (учебное) время')
-    markup.row(btn3) #, btn4
+    markup.row(btn3, btn4) #, btn4
     # markup.add(btn1, btn2, btn3, btn4) - просто добавить кнопки в markup
     bot.send_message(message.chat.id, 'Привет. На какой день недели хочешь узнать дз?)', reply_markup = markup)
 
@@ -163,6 +164,127 @@ def on_click(message):
                          'Номер телефона, к которому привязана карта:\n'
                          '+79504342736\n'
                          'Также желательно либо в комментарии к переводу либо в лс писать кто сделал перевод, чтобы знал кто сделал мне приятно и кому нужно быть бесконечно благодарным!)')
+    # Статья про вложенные кнопки https: // habr.com / ru / sandbox / 163347 /
+    elif message.text.lower() == 'расписание':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Понедельник')
+        btn2 = types.KeyboardButton('Вторник')
+        btn3 = types.KeyboardButton('Среда')
+        btn4 = types.KeyboardButton('Четверг')
+        markup.row(btn1, btn2, btn3, btn4)
+        btn5 = types.KeyboardButton('Пятница')
+        btn6 = types.KeyboardButton('Суббота')
+        btn7 = types.KeyboardButton('Вся неделя')
+        btn8 = types.KeyboardButton('Назад')
+        markup.row(btn5, btn6, btn7, btn8)
+        bot.send_message(message.chat.id, 'Выбери день недели, на который хочешь узнать среднестатистическое расписание', reply_markup = markup)
+    elif message.text.lower() == 'понедельник':
+        bot.send_message(message.chat.id,
+                         '1. Разговор о важном\n'
+                         '2. Литература\n'
+                         '3. История\n'
+                         '4. Литература\n'
+                         '5. Физика\n'
+                         '6. Информатика')
+    elif message.text.lower() == 'вторник':
+        bot.send_message(message.chat.id,
+                         '1. Информатика\n'
+                         '2. Информатика\n'
+                         '3. Астрономия\n'
+                         '4. Русский язык\n'
+                         '5. Профильная Математика\n'
+                         '6. Профильная Математика\n'
+                         '7. Профильная Математика')
+    elif message.text.lower() == 'среда':
+        bot.send_message(message.chat.id,
+                         'Постоянно меняется порядок\n'
+                         '1. Физика\n'
+                         '2. Английский язык\n'
+                         '3. Родной язык\n'
+                         '4. Информатика\n'
+                         '5. Профильная Математика\n'
+                         '6. Профильная Математика')
+    elif message.text.lower() == 'четверг':
+        bot.send_message(message.chat.id,
+                         'Постоянно меняется порядок\n'
+                         '1. Профильная Математика\n'
+                         '2. Физика\n'
+                         '3. Литература\n'
+                         '4. Информатика\n'
+                         '5. Профминимум\n'
+                         '6. Физкультура\n'
+                         '7. Физкультура')
+    elif message.text.lower() == 'пятница':
+        bot.send_message(message.chat.id,
+                         'Постоянно меняется порядок\n'
+                         '1. История\n'
+                         '2. Физика\n'
+                         '3. Английский язык\n'
+                         '4. Информатика\n'
+                         '5. Русский язык\n'
+                         '6. Физкультура\n'
+                         '7. Профильная математика')
+    elif message.text.lower() == 'суббота':
+        bot.send_message(message.chat.id,
+                         '1. нет урока\n'
+                         '2. Физика\n'
+                         '3. Химия\n'
+                         '4. ОБЖ')
+    elif message.text.lower() == 'вся неделя':
+        bot.send_message(message.chat.id,
+                         'Пн Разговор о важном\n'
+                         'Пн Литература\n'
+                         'Пн История\n'
+                         'Пн Литература\n'
+                         'Пн Физика\n'
+                         'Пн Информатика\n'
+                         '\n'
+                         'Вт Информатика\n'
+                         'Вт Информатика\n'
+                         'Вт Астрономия\n'
+                         'Вт Русский язык\n'
+                         'Вт Профильная Математика\n'
+                         'Вт Профильная математика\n'
+                         'Вт Профильная математика\n'
+                         '\n'
+                         'Ср Физика\n'
+                         'Ср Английский язык\n'
+                         'Ср Родной язык\n'
+                         'Ср Информатика\n'
+                         'Ср Профильная математика\n'
+                         'Ср Профильная математика\n'
+                         '\n'
+                         'Чт Математика\n'
+                         'Чт Физика\n'
+                         'Чт Литература\n'
+                         'Чт Информатика\n'
+                         'Чт Профминимум\n'
+                         'Чт Физкультура\n'
+                         'Чт Физкультура\n'
+                         '\n'
+                         'Пт История\n'
+                         'Пт Физика\n'
+                         'Пт Английский язык\n'
+                         'Пт Информатика\n'
+                         'Пт Русский язык\n'
+                         'Пт Физкультура\n'
+                         'Пт Математика\n'
+                         '\n'
+                         'Сб нет урока\n'
+                         'Сб Физика\n'
+                         'Сб Химия\n'
+                         'Сб ОБЖ\n'
+                         '\n'
+                         'Это среднестатистическое расписание. В Ср, Чт, Пт и Сб порядок уроков может меняться.')
+    elif message.text.lower() == 'назад':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Завтра')
+        btn2 = types.KeyboardButton('Другая дата')
+        markup.row(btn1, btn2)
+        btn3 = types.KeyboardButton('Донат')
+        btn4 = types.KeyboardButton('Расписание')
+        markup.row(btn3, btn4)
+        bot.send_message(message.chat.id, 'Привет. На какой день недели хочешь узнать дз?)', reply_markup=markup)
     # Обработка кнопки 'Другая дата'
     elif check():
         read_dz_technical(message.text)
@@ -186,6 +308,8 @@ def on_click(message):
                          '-- Маяковский: 1 любое на выбор\n'
                          'Информатика: есть в вк в группе "11 проф. математика 134"\n'
                          'Физика: есть в тг в группе "11 ЕГЭ"')
+    elif 'спасибо' in message.text.lower():
+        bot.send_message(message.chat.id, 'Всегда пожалуйста!)')
     # Ответ на неопределённое сообщение
     else:
         bot.send_message(message.chat.id, 'К сожалению, я не знаю как отвечать на такое сообщение( Обратись к разработчику (команда "/help") и опиши свою проблему, чтобы я совершенствовался и был ещё лучше для вас)')
